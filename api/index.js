4d675769a71ba372292ad1f1c017399766924293
linux <sa5882218@gmail.com>
2025-04-16 11:16:09 +0500

    remve servberless

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const serverless = require('serverless-http');
+// const serverless = require('serverless-http');
 const path = require('path');
 const categoryRoutes = require('../routes/categoryRoutes');
 const userRoutes = require('../routes/userRoutes');
@@ -9,30 +9,29 @@ const orderRoutes = require('../routes/orderRoutes');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-// Enable CORS
+
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
 
-// Parse JSON
+
 app.use(bodyParser.json());
 app.use(express.json());
 
-// Serve static files (Consider using cloud storage for production)
+// Use the route
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
-
-// Basic route for health check
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
-
-// Use Routes
 app.use('/users', userRoutes);
 app.use('/products', productRoutes);
 app.use('/categories', categoryRoutes);
 app.use('/orders', orderRoutes);
 
-// Export serverless handler
-module.exports.handler = serverless(app);
+// Start server
+ app.listen(process.env.PORT || 4000, () => {
+   console.log('Server running on http://localhost:3000');
+ });
+
